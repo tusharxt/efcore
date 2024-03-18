b5d1bb7d5be82bec3e04af0c6289fe82966bff8a
tusharxt <tushar3940singh@gmail.com>
2024-03-18 22:36:57 +0530

    arithmetic branch commited

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,20 @@
             int a= int.Parse(Console.ReadLine());
             Console.WriteLine("1st no is : "+a);
             //int a =
+            Console.WriteLine("enter 2nd no");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("2nd no is : " + b);
 
+            Console.WriteLine("Enter 1 to perform addition and 2 to perform substraction");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+                Console.WriteLine(a + b);
+            else if (choice == 2)
+            {
+                int ans = a > b ? (a - b) : (b - a);
+                Console.WriteLine(ans);
+            }
+            else { Console.WriteLine("Invalid input"); }
         }
     }
 }
